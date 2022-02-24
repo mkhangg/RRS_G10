64366971e7cb0dc1fc3d16fef4f89a834a6a64a7
Khang Nguyen <46115541+mkhangg@users.noreply.github.com>
2022-02-24 14:53:52 -0600

    Create query9b.sql

---
+++ SQL_Queries/query9b.sql
@@ -0,0 +1,9 @@
+SELECT distinct PASSENGER.Fname, PASSENGER.Lname, PASSENGER.Ssn, TICKET.Ticket_category
+FROM TRAIN, PASSENGER, TICKET
+WHERE TRAIN.Weekday LIKE '%Saturday%' AND TRAIN.Train_num = TICKET.Train_num AND TICKET.Ssn = PASSENGER.Ssn AND TICKET.Ticket_category = 'Premium'
+
+INTERSECT
+
+SELECT distinct PASSENGER.Fname, PASSENGER.Lname, PASSENGER.Ssn, TICKET.Ticket_category
+FROM TRAIN, PASSENGER, TICKET
+WHERE TRAIN.Weekday LIKE '%Sunday%' AND TRAIN.Train_num = TICKET.Train_num AND TICKET.Ssn = PASSENGER.Ssn AND TICKET.Ticket_category = 'Premium';
